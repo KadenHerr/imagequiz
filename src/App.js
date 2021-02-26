9555ddf5234dc4df5d29f339e82aa4473c623613
KadenHerr <kherr@email.arizona.edu>
2021-02-26 14:58:24 -0700

    Skeleton Layout complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter,Switch,Route} from "react-router-dom";
+
+import Home from './components/Home';
+import Login from './components/Login';
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <HashRouter>
+      <NavigationBar />
+      <Switch>
+        <Route path="/home" component={Home}/>
+        <Route path="/login" component={Login}/>
+      </Switch>
+    </HashRouter>
   );
 }
 
