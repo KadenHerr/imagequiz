fc631a155b1eb622a9431c761a126249a65b455d
KadenHerr <kherr@email.arizona.edu>
2021-02-26 18:38:12 -0700

    Bare bones created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import NavigationBar from "./components/NavigationBar";
 
+
 function App() {
   return (
     <HashRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
+import Data from "./Data";
 
 
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <h2>Home</h2>
+            <Data
+            src="../images/something.jpg"
+            alt="Picture of something"
+            byline="This is a picture of something."/>
+
+        </div>
     );
 }
 
